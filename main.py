5defe1e94cbf8a6966fccfad3e1530810dca258c
mathieu.chambaud@ynov.com <mathieu.chambaud@ynov.com>
2020-02-07 17:13:26 +0100

    dernier commit code en cours...

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-import imaplib, serial, struct, time, json
+import imaplib, serial, struct, time
+import json
 
-with open("conf.json") as config:
+with open("config.json") as config:
     print(type(config))
+
+# Check d'un nouveau mail toutes les minutes 
 
 class Mail():
     def __init__(self):
-        self.user= 'mathchambaud2000@gmail.com'
-        self.password= 'Mathieu2018!'
+        self.user= config.senderMail
+        self.password= config.Password
         self.ser = serial.Serial('/dev/tty.usbmodem621', 9600)
-        self.M = imaplib.IMAP4_SSL('imap.gmail.com', '993')
+        self.M = imaplib.IMAP4_SSL(config.domaineServer, '993')
         self.M.login(self.user, self.password)      
     def checkMail(self):
         self.M.select()
@@ -18,10 +21,15 @@ class Mail():
         self.numMessages= self.checkMail()
         #turn the string into packed binary data to send int
         self.ser.write(struct.pack('B', self.numMessages))       
-email = Mail()
-# Check d'un nouveau mail toutes les minutes 
-while 1:
-    print('Sending')
-    email.sendData()
-    time.sleep(60)
+
+def main():
+        
+    email = Mail()
     
+    while 1:
+        print('Sending')
+        email.sendData()
+        time.sleep(60)
+ 
+if __name__ == "__main__":
+    main()
