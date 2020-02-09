c0af5155b7fe85e7f915ebae69e4b46618b22335
FrekiManagarm <mathieu.chambaud@ynov.com>
2020-02-09 22:37:08 +0100

    dernière ligne droite

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ import json
 
 with open('config.json') as config_data:
     print(type(config_data))
-
+email = Mail()
 # Check d'un nouveau mail toutes les minutes 
+
+def main():
+
 
 class Mail():
     def __init__(self):
@@ -21,15 +24,21 @@ class Mail():
         self.numMessages= self.checkMail()
         #turn the string into packed binary data to send int
         self.ser.write(struct.pack('B', self.numMessages))
-
-def main():
-        
-    email = Mail()
-    
-    while 1:
+while 1:
         print('Sending')
         email.sendData()
         time.sleep(60)
+
+class Translation():
+    def __init__(self):
+        with open("new_mail", "r") as Mail_data:
+            print(type(Mail_data))
+        Mail_data = {email.content}
+        fichier = open(".json","wt")
+        fichier.write(json.dumps(Mail_data))
+        fichier.close()
+
+
  
 if __name__ == "__main__":
     main()
